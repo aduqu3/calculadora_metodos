224bd7933f94d4d0204696ebe67301e7865f62b2
aduqu3 <andres1duque@gmail.com>
2021-09-10 03:58:04 -0500

    update datra set

--- data_set.py
+++ data_set.py
@@ -4,11 +4,13 @@ x^3 + 4*(x^2) - 10
 2
 0.001
 
+
 # secante
 x^3 + 4*x^2 - 10
 2
 1.5
 0.001
+
 
 # punto fijo
 x^2 -3*x + 4
@@ -16,6 +18,46 @@ x^2 -3*x + 4
 0.0001
 100
 
+
 # errores
 2
 2.3  
+
+
+# simpson 1/3
+sin(2*pi*x)+x^2
+n = 100
+a = 0.0
+b = 1.0
+
+
+# trapecios
+a = 1
+b = 2
+m = 4
+sin(2*pi*x)+x^2
+
+
+# simpson 3/8
+x^2 - x - 1
+x0=1
+xf=2
+n=3
+
+
+# biseccion
+x2 - x - 1
+>>> bisection(f,1,2,25)
+1.618033990263939
+# ----
+(2*x - 1)*(x - 3)
+>>> bisection(f,0,1,10)
+0.5
+
+
+# falsa posicion
+x^3 + 4*x^2 - 10
+a = 1
+b = 2
+tolera = 0.001
+
