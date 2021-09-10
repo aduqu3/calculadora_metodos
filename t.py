a8c6e451dbd1230e2d474b673a96b6ec01121701
aduqu3 <andres1duque@gmail.com>
2021-09-10 03:09:45 -0500

    updated punto fijo

--- t.py
+++ t.py
@@ -1,11 +1,11 @@
-#========================= WINDOW METODO SECANTE ========================================= INI
-def window_secante():
-    global window_7
+#========================= WINDOW METODO PUNTO FIJO ========================================= INI
+def window_punto_fijo():
+    global window_8
    
-    window_7 = tk.Toplevel(root)
-    frame_7 = scroll_bar(window_7)
-    window_7.geometry("500x500")
-    window_7.title("Secante")
+    window_8 = tk.Toplevel(root)
+    frame_8 = scroll_bar(window_8)
+    window_8.geometry("500x500")
+    window_8.title("Punto Fijo")
 
     def press(func_d, a_d, b_d, n_d,):
         func = func_d.get()
@@ -15,23 +15,23 @@ def window_secante():
 
         if( len(func) and len(a) and len(b) and len(n)):
             # print("vamos a hacer algo")
-            a = int(a)
+            a = float(a)
             b = float(b)
-            n = float(n)
+            n = int(n)
             
             # procesar la info y vaciar el StringVar tk_string, para que no se muestre nuevamente
             # la funcion ingresada con anterioridad
            
             # btn graficar ecuacion
-            btn_graph = ttk.Button(frame_7, text="Graficar", command=lambda: graph(func, 20, a, 20))
+            btn_graph = ttk.Button(frame_8, text="Graficar", command=lambda: graph(func, int(n), int(a), 20))
             btn_graph.pack(pady=10)
 
-            result_lbl = tk.Label(frame_7, text=('Resultado Newton Rapshon: ', secante(func, b, n)), font=("Helvetica", 14))
+            result_lbl = tk.Label(frame_8, text=('Punto Fijo solucion: x ', punto_fijo(func, a, b, n)), font=("Helvetica", 14))
             result_lbl.pack(fill = BOTH, pady=10)
 
             # btn para limpiar la interfaz luego de realizar un calculo
             var = tk.IntVar()
-            btn_clean = ttk.Button(frame_7, text="Limipiar", command=lambda: var.set(1))
+            btn_clean = ttk.Button(frame_8, text="Limipiar", command=lambda: var.set(1))
             btn_clean.pack(pady=10)
             # btn_clean.place(relx=.5, rely=.5, anchor="c")
 
@@ -44,7 +44,7 @@ def window_secante():
             result_lbl.destroy()
             btn_clean.destroy()
             # btn_clean.destroy()
-            # frame_7.destroy()
+            # frame_8.destroy()
             func = ''
             n = ''
             a = ''
@@ -56,74 +56,70 @@ def window_secante():
         else:
             print("vacio: por favor ingrese todos los campos")
             
-    title_lbl = tk.Label(frame_7, text="Secante\n",
+    title_lbl = tk.Label(frame_8, text="Punto Fijo\n",
     font=("Helvetica", 14), justify="left")
     title_lbl.pack(fill = BOTH)
 
-    lbl = tk.Label(frame_7, text="El método de Newton-Rhapson es uno de los más utilizados para dar solución a \necuaciones algebraicas y trascendentes. Clasificándose entre los métodos abiertos, es decir, \naquellos que requieren uno o dos valores que no necesariamente encierren a la raíz.\n\n El método de Newton-Raphson se deduce a partir de esta interpretación geométrica y \nse tiene que la primera derivada en x es equivalente a la pendiente:",
+    lbl = tk.Label(frame_8, text="El principal inconveniente del método de Newton estriba en que requiere conocer \nel valor de la primera derivada de la función en el punto. Sin embargo, \nla forma funcional de f(x) dificulta en ocasiones el cálculo de la derivada. \nEn estos casos es más útil emplear el método de la secante.\n\nEl método de la secante parte de dos puntos (y no sólo uno como el método de Newton)\ny estima la tangente (es decir, la pendiente de la recta) por una aproximación \nde acuerdo con la expresión:",
     font=("Helvetica", 11), justify="left")
     lbl.pack(fill = BOTH)
 
-    img = add_img("assets/newton_raphson/1.png", frame_7, 10)
+    img = add_img("assets/secante/1.png", frame_8, 10)
 
-    lbl2 = tk.Label(frame_7, text="Que se arregla para obtener", font=("Helvetica", 11), justify="left")
+    lbl2 = tk.Label(frame_8, text="Sustituyendo esta expresión en la ecuación (29) del método de Newton, obtenemos \nla expresión del método de la secante que nos proporciona el siguiente punto de iteración:", font=("Helvetica", 11), justify="left")
     lbl2.pack(fill=BOTH, pady=10)
 
-    img2 = add_img("assets/newton_raphson/2.png", frame_7, 10)
-
-    lbl3 = tk.Label(frame_7, text="Mientras que el error aproximado porcentual lo calcularemos con la siguiente fórmula:", font=("Helvetica", 11), justify="left")
-    lbl3.pack(fill=BOTH, pady=10)
-
-    img3 = add_img("assets/newton_raphson/1.png", frame_7, 10)
+    img2 = add_img("assets/secante/2.png", frame_8, 10)
+    img3 = add_img("assets/secante/3.png", frame_8, 10)
     
-    test_lbl = tk.Label(frame_7, text="Probar metodo:", font=("Helvetica", 11), justify="left")
+    test_lbl = tk.Label(frame_8, text="Probar metodo:", font=("Helvetica", 11), justify="left")
     test_lbl.pack(fill = BOTH, pady=10)
 
     # func
     # colocar label para el input
-    func_lbl = ttk.Label(frame_7, text = "f:", font=("Helvetica", 11), justify="left")
+    func_lbl = ttk.Label(frame_8, text = "funcion:", font=("Helvetica", 11), justify="left")
     func_lbl.pack()
     # se crea un entry, para el ingreso de texto desde teclado
     # luego guardamos esa informacion dentro de un StringVar tk_string
     func_str = tk.StringVar()
-    func_inp = ttk.Entry(frame_7, textvariable=func_str)
+    func_inp = ttk.Entry(frame_8, textvariable=func_str)
     func_inp.configure(background="white")
     func_inp.focus()
     func_inp.pack(pady=10)
     
     # a
-    a_lbl = ttk.Label(frame_7, text = "a", font=("Helvetica", 11))
+    a_lbl = ttk.Label(frame_8, text = "aprox:", font=("Helvetica", 11))
     a_lbl.pack()
     
     a_str = tk.StringVar()
-    a_inp = ttk.Entry(frame_7, textvariable=a_str)
+    a_inp = ttk.Entry(frame_8, textvariable=a_str)
     a_inp.configure(background="white")
     a_inp.focus()
     a_inp.pack(pady=10)
     # 
 
     # b
-    b_lbl = ttk.Label(frame_7, text = "xa:", font=("Helvetica", 11))
+    b_lbl = ttk.Label(frame_8, text = "tol:", font=("Helvetica", 11))
     b_lbl.pack(pady=10)
     
     b_str = tk.StringVar()
-    b_inp = ttk.Entry(frame_7, textvariable=b_str)
+    b_inp = ttk.Entry(frame_8, textvariable=b_str)
     b_inp.configure(background="white")
     b_inp.focus()
     b_inp.pack(pady=10)
     # 
 
     # n
-    n_lbl = ttk.Label(frame_7, text = "tolerancia:", font=("Helvetica", 11))
+    n_lbl = ttk.Label(frame_8, text = "n:", font=("Helvetica", 11))
     n_lbl.pack(pady=10)
     
     n_str = tk.StringVar()
-    n_inp = ttk.Entry(frame_7, textvariable=n_str)
+    n_inp = ttk.Entry(frame_8, textvariable=n_str)
     n_inp.configure(background="white")
     n_inp.focus()
     n_inp.pack(pady=10)
     # 
         
-    submit_btn = ttk.Button(frame_7, text = "calcular", command=lambda: press(func_str, a_str, b_str, n_str))
+    submit_btn = ttk.Button(frame_8, text = "calcular", command=lambda: press(func_str, a_str, b_str, n_str))
     submit_btn.pack(pady=10)
-#========================= WINDOW METODO SECANTE ========================================= FIN
+#========================= WINDOW METODO PUNTO FIJO ========================================= FIN
--- tutorial.py
+++ tutorial.py
@@ -1029,12 +1029,17 @@ def window_secante():
 def window_punto_fijo():
     global window_8
    
+    window_8 = tk.Toplevel(root)
+    frame_8 = scroll_bar(window_8)
+    window_8.geometry("500x500")
+    window_8.title("Punto Fijo")
+
     def press(func_d, a_d, b_d, n_d,):
         func = func_d.get()
         a = a_d.get()
-        b = b_d.get()
-        n = n_d.get()
-        
+        b = b_d.get() #  xa float
+        n = n_d.get() # tolaera float
+
         if( len(func) and len(a) and len(b) and len(n)):
             # print("vamos a hacer algo")
             a = float(a)
@@ -1045,17 +1050,16 @@ def window_punto_fijo():
             # la funcion ingresada con anterioridad
            
             # btn graficar ecuacion
-            btn_graph = ttk.Button(window_8, text="Graficar", command=lambda: graph(func, int(n), int(a), 20))
-            btn_graph.pack()
+            btn_graph = ttk.Button(frame_8, text="Graficar", command=lambda: graph(func, int(n), int(a), 20))
+            btn_graph.pack(pady=10)
 
-            # 
-            result_lbl = tk.Label(window_8, text=('Punto Fijo solucion: x ', punto_fijo(func, a, b, n)))
-            result_lbl.pack(fill = BOTH)
+            result_lbl = tk.Label(frame_8, text=('Punto Fijo solucion: x ', punto_fijo(func, a, b, n)), font=("Helvetica", 14))
+            result_lbl.pack(fill = BOTH, pady=10)
 
             # btn para limpiar la interfaz luego de realizar un calculo
             var = tk.IntVar()
-            btn_clean = ttk.Button(window_8, text="Limipiar", command=lambda: var.set(1))
-            btn_clean.pack()
+            btn_clean = ttk.Button(frame_8, text="Limipiar", command=lambda: var.set(1))
+            btn_clean.pack(pady=10)
             # btn_clean.place(relx=.5, rely=.5, anchor="c")
 
             # print("waiting...")
@@ -1067,7 +1071,7 @@ def window_punto_fijo():
             result_lbl.destroy()
             btn_clean.destroy()
             # btn_clean.destroy()
-            # window_1.destroy()
+            # frame_8.destroy()
             func = ''
             n = ''
             a = ''
@@ -1079,61 +1083,66 @@ def window_punto_fijo():
         else:
             print("vacio: por favor ingrese todos los campos")
             
+    title_lbl = tk.Label(frame_8, text="Punto Fijo\n",
+    font=("Helvetica", 14), justify="left")
+    title_lbl.pack(fill = BOTH)
 
-    window_8 = tk.Toplevel(root)
-    window_8.geometry("800x400")
-    window_8.title("Punto Fijo")
-    
-    lbl = tk.Label(window_8, text="El metodo de Punto Fijo...")
+    lbl = tk.Label(frame_8, text="Dada una ecuación f(x) = 0, podemos transformarla, de alguna manera, en otra \nequivalente del tipo x = g(x) para alguna función g. En este caso se tiene que: \na es raíz de f(x) = 0 ↔ f(a) = 0 ↔ a = g(a) ↔ a es raíz de x = g(x).\n\nDefinición:\n\nUn número a tal que a = g(a) se dice un punto fijo de la función g.\nCuándo una función g tiene un punto fijo, y si lo tiene, cómo encontrarlo?\n\nTeorema de punto fijo:\n\nSi g es una función continua en [a, b] y g(x) ε[a, b] para todo x ε[a, b], \nentonces g tiene por lo menos un punto fijo en [a, b]. Si además, \ng’(x) existe para todo x ε[a, b], y |g’(x)| ≤ K < 1 para todo x ε[a, b], K constante, \nentonces g tiene un único punto fijo x ε[a, b]. La sucesión {xn}, con n definida, \nse encuentra mediante la fórmula de iteración:",
+    font=("Helvetica", 11), justify="left")
     lbl.pack(fill = BOTH)
+
+    img = add_img("assets/punto_fijo/1.png", frame_8, 10, 2, 2)
+    
+    test_lbl = tk.Label(frame_8, text="Probar metodo:", font=("Helvetica", 11), justify="left")
+    test_lbl.pack(fill = BOTH, pady=10)
 
     # func
     # colocar label para el input
-    func_lbl = ttk.Label(window_8, text = "funcion:")
+    func_lbl = ttk.Label(frame_8, text = "funcion:", font=("Helvetica", 11), justify="left")
     func_lbl.pack()
     # se crea un entry, para el ingreso de texto desde teclado
     # luego guardamos esa informacion dentro de un StringVar tk_string
     func_str = tk.StringVar()
-    func_inp = ttk.Entry(window_8, textvariable=func_str)
+    func_inp = ttk.Entry(frame_8, textvariable=func_str)
     func_inp.configure(background="white")
     func_inp.focus()
-    func_inp.pack()
+    func_inp.pack(pady=10)
     
     # a
-    a_lbl = ttk.Label(window_8, text = "aprox:")
+    a_lbl = ttk.Label(frame_8, text = "aprox:", font=("Helvetica", 11))
     a_lbl.pack()
     
     a_str = tk.StringVar()
-    a_inp = ttk.Entry(window_8, textvariable=a_str)
+    a_inp = ttk.Entry(frame_8, textvariable=a_str)
     a_inp.configure(background="white")
     a_inp.focus()
-    a_inp.pack()
+    a_inp.pack(pady=10)
     # 
 
     # b
-    b_lbl = ttk.Label(window_8, text = "tol:")
-    b_lbl.pack()
+    b_lbl = ttk.Label(frame_8, text = "tol:", font=("Helvetica", 11))
+    b_lbl.pack(pady=10)
     
     b_str = tk.StringVar()
-    b_inp = ttk.Entry(window_8, textvariable=b_str)
+    b_inp = ttk.Entry(frame_8, textvariable=b_str)
     b_inp.configure(background="white")
     b_inp.focus()
-    b_inp.pack()
+    b_inp.pack(pady=10)
     # 
 
     # n
-    n_lbl = ttk.Label(window_8, text = "n:")
-    n_lbl.pack()
+    n_lbl = ttk.Label(frame_8, text = "n:", font=("Helvetica", 11))
+    n_lbl.pack(pady=10)
     
     n_str = tk.StringVar()
-    n_inp = ttk.Entry(window_8, textvariable=n_str)
+    n_inp = ttk.Entry(frame_8, textvariable=n_str)
     n_inp.configure(background="white")
     n_inp.focus()
-    n_inp.pack()
+    n_inp.pack(pady=10)
     # 
-    
-    submit_btn = ttk.Button(window_8, text = "calcular", command=lambda: press(func_str, a_str, b_str, n_str))
-    submit_btn.pack()
+        
+    submit_btn = ttk.Button(frame_8, text = "calcular", command=lambda: press(func_str, a_str, b_str, n_str))
+    submit_btn.pack(pady=10)
 #========================= WINDOW METODO PUNTO FIJO ========================================= FIN
 
 
